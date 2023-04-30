fe959888f835e0ad5f778f21d6c47a6e773fe292
seolah97 <sbyun@appdev.yearup.org>
2023-04-30 19:42:42 -0400

    Working on Checkout. Almost at finishline :)

--- src/main/java/com/sb/Checkout.java
+++ src/main/java/com/sb/Checkout.java
@@ -1,15 +1,22 @@
 package com.sb;
 
+import static com.sb.OnlineStore.cart;
+
 public class Checkout {
     // Create static method called checkout that return nothing
     public static void checkout() {
+        int cartSize = cart.size();
         // Create a variable to store the sum of the cart
-        // Iterate through the cart
-        // Add each product price to the sum variable
+        float cartSum = 0;
+        for (Products products : cart) {
+        // Add each item price to the sum variable
+        cartSum += products.getPrice();
+        }
         // Print out the total owed(sum variable) by the user
-
+        System.out.printf("Your Subtotal is (%d Item): $%.2f\n", cartSize, cartSum);
         // Print a message to the user to pay for the items
-        // Create a variable and prompt from the user for an int that represents how much they are willing to pay in cash
+        System.out.println("Please pay for your purchase with cash.");
+        System.out.println("Enter the payment amount: $");
 
         // If the amount given by the user covers the entire amount owed
         // Print out the difference that is left over
