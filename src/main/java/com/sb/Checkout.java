acfdceef372db96f468b2b45bb0cb37d1fa7e7e3
seolah97 <sbyun@appdev.yearup.org>
2023-05-01 00:29:12 -0400

    --__-- mmm.. basically finished? Store works

--- src/main/java/com/sb/Checkout.java
+++ src/main/java/com/sb/Checkout.java
@@ -40,15 +40,28 @@ public class Checkout {
             System.out.println("The amount provided does not cover your total cost.");
             // This lets user know that the amount is not enough, and what is left to pay.
 
-            do{ // This will make it run until full amount is paid
+            float remainingAmount;
+            do { // This will make it run until full amount is paid
                 System.out.printf("The payment was not enough to cover the full amount, " +
-                    "finish paying the remaining amount of $%.2f", Math.abs(difference));
+                        "finish paying the remaining amount of $%.2f", Math.abs(difference));
 
-                System.out.println("Enter the payment amount: $");
-                scanner.nextFloat();
+                System.out.print("\nEnter the payment amount: $");
+                remainingAmount = scanner.nextFloat();
 
+                // If the amount given by the user covers the entire amount owed
 
-            } while (paymentAmount != 0);
+                if (remainingAmount > cartSum) {
+                    difference = remainingAmount - cartSum;
+                    // Print out the difference that is left over
+                    System.out.printf("Purchase Successful! Your change is $%.2f\n", difference);
+                    // Print all the items that the user has purchased
+                    System.out.println("Items purchased:");
+                    for (Products products : cart) {
+                        System.out.print(products.toString());
+                    }
+                    cart.clear(); // Clear the cart arraylist
+                }
+            } while (remainingAmount != 0);
             scanner.close();
         }
     }
---
+++ src/test/README.md
@@ -0,0 +1,3 @@
+Working on having a partial payment system. Working out the kinks.
+
+It will register the first partial payment, but does not calibrate again with the new remaining amount if it is another partial payment or has paid off the full payment amount.
