ba7739d25025424f1f84e16a4f9925ea9a605331
seolah97 <sbyun@appdev.yearup.org>
2023-04-30 23:51:49 -0400

    On the right track. prompts up to asking for remainder amount to pay

--- src/main/java/com/sb/Checkout.java
+++ src/main/java/com/sb/Checkout.java
@@ -10,8 +10,8 @@ public class Checkout {
         // Create a variable to store the sum of the cart
         float cartSum = 0;
         for (Products products : cart) {
-        // Add each item price to the sum variable
-        cartSum += products.getPrice();
+            // Add each item price to the sum variable
+            cartSum += products.getPrice();
         }
         // Print out the total owed(sum variable) by the user
         System.out.printf("Your Subtotal is (%d Item): $%.2f\n", cartSize, cartSum);
@@ -22,14 +22,14 @@ public class Checkout {
         float paymentAmount = scanner.nextFloat();
 
         // If the amount given by the user covers the entire amount owed
-        float difference = 0;
+        float difference = (paymentAmount - cartSum);
         if (paymentAmount >= cartSum) {
             difference = paymentAmount - cartSum;
             // Print out the difference that is left over
             System.out.printf("Purchase Successful! Your change is $%.2f\n", difference);
             // Print all the items that the user has purchased
             System.out.println("Items purchased:");
-            for (Products products : cart){
+            for (Products products : cart) {
                 System.out.print(products.toString());
             }
             cart.clear(); // Clear the cart arraylist
@@ -38,13 +38,18 @@ public class Checkout {
         } else { // Otherwise
             // Let the user know that the amount given doesn't cover the full amount
             System.out.println("The amount provided does not cover your total cost.");
-            float amountRemaining = 0;
-            amountRemaining = paymentAmount - difference;
-            System.out.printf("The payment was not enough to cover the full amount, " +
-                    "finish paying the remaining amount of $%.2f", amountRemaining);
+            // This lets user know that the amount is not enough, and what is left to pay.
+
+            do{ // This will make it run until full amount is paid
+                System.out.printf("The payment was not enough to cover the full amount, " +
+                    "finish paying the remaining amount of $%.2f", Math.abs(difference));
+
+                System.out.println("Enter the payment amount: $");
+                scanner.nextFloat();
 
 
+            } while (paymentAmount != 0);
+            scanner.close();
         }
-        scanner.close();
     }
 }
